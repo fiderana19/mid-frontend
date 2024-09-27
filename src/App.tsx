93a807803c2460c9792f4c659668b313cbd32dfe
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-09-27 10:32:51 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="text-5xl">It's me, Mario !!!</div>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
