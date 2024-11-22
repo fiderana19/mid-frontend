bfc28bc2bac4c918dd92a12a6e5d343024437ad7
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-11-22 16:32:46 +0300

    feat: Fixed the color in admin nav

--- src/components/MidProfile.tsx
+++ src/components/MidProfile.tsx
@@ -5,7 +5,7 @@ const MidProfile: FunctionComponent = () => {
     return(
         <div className="">
             <img src={MidLogo} alt="Logo du ministere" className="w-10 h-10 md:w-20 md:h-20 object-cover rounded-full mx-auto" />
-            <div className="text-md font-semibold mt-2 md:block hidden">MININTER: Audience</div>
+            <div className="text-md font-latobold text-white mt-2 md:block hidden">MININTER: Audience</div>
         </div>
     )
 }
--- src/components/Midopyright.tsx
+++ src/components/Midopyright.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 const MidCopyright: FunctionComponent = () => {
     return(
         <div>
-            <div className="text-xs font-bold md:block hidden">
+            <div className="text-xs font-latobold text-white md:block hidden">
                 MININTER. @copyright 2024
             </div>
         </div>
