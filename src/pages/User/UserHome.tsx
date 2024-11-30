4d58b28a40b6b1b7b27244393595191d23da7ca6
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-11-30 16:11:18 +0300

    feat: Fixed the responsive in user home

--- src/pages/User/UserHome.tsx
+++ src/pages/User/UserHome.tsx
@@ -26,23 +26,25 @@ const UserHome: FunctionComponent = () => {
     return(
         <div className="w-full flex flex-col justify-center bg-four min-h-screen">
             <UserNavigation />
-            <div className="pt-16 sm:px-20 px-4">
+            <div className="pt-16 lg:px-20 px-4">
                 {
                 user && 
-                    <div className="sm:flex block items-center gap-5">
-                        <div className="sm:w-1/4 w-full">
+                    <div className="md:flex block items-center lg:gap-5 gap-2 md:my-0 my-4">
+                        <div className="md:w-1/4 w-full">
                             <img src={`data:image/png;base64,${user.profile_photo}`} className="w-52 h-52 object-cover border mx-auto" />
                         </div>
-                        <div className="sm:w-2/4 w-full sm:text-left text-center sm:my-0 my-4">
-                            <div className="text-5xl font-bold whitespace-normal">
+                        <div className="md:w-2/4 w-full md:text-left text-center md:my-0 my-4">
+                            <div className="sm:text-5xl text-2xl font-bold whitespace-normal">
                                 <Typewriter text={`Bonjour, ${user.nom} ${user.prenom}`} />
                             </div>
                         </div>
-                        <div className="sm:w-1/4 w-full bg-white shadow-md rounded">
-                            <img src={Ministre} className="w-full object-cover border mx-auto" />
-                            <div className="p-2">
-                                <div className="text-center text-lg font-latobold"> Madame { ministre.nom } {ministre.prenom}</div>
-                                <div className="text-center text-md text-gray-800"> Ministre de l'Intérieur</div>
+                        <div className="md:w-1/4 w-full">
+                            <div className="sm:w-2/3 md:w-full w-full bg-white shadow-md rounded mx-auto">
+                                <img src={Ministre} className="w-full object-cover border mx-auto" />
+                                <div className="p-2">
+                                    <div className="text-center text-lg font-latobold"> Madame { ministre.nom } {ministre.prenom}</div>
+                                    <div className="text-center text-md text-gray-800"> Ministre de l'Intérieur</div>
+                                </div>
                             </div>
                         </div>
                     </div>
