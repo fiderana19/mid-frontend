4d50484b2fbf12d257dfaec38312316c571b7ad0
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-12-05 17:32:06 +0300

    feat: Fixed adresse input error

--- src/pages/Signup/SignupPersonnal.tsx
+++ src/pages/Signup/SignupPersonnal.tsx
@@ -9,15 +9,20 @@ interface StepsProp {
 
 const SignupPersonnal: FunctionComponent<StepsProp> = ({formData, handleNext, handleChange}) => {
     const [nomError, setNomError] = useState<string>('');
+    const [adresseError, setAdresseError] = useState<string>('');
 
     async function handleSubmit () {
         setNomError('');
+        setAdresseError('');
         
         if(formData.nom === '') {
             setNomError("Le champ nom ne doit pas être vide !");
         }
+        if(formData.adresse === '') {
+            setAdresseError("Le champ adresse ne doit pas être vide !");
+        }
 
-        if(formData.nom !== "") {
+        if(formData.nom !== "" && formData.adresse !== "") {
             handleNext();
         }
     }
@@ -65,10 +70,11 @@ const SignupPersonnal: FunctionComponent<StepsProp> = ({formData, handleNext, ha
                         onChange={handleChange} 
                         name="adresse"
                         placeholder="Saisir votre adresse..."
-                        className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pr-3 pl-10 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                        className={adresseError ? "border-red-500 w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border rounded-md pr-3 pl-10 py-2 transition duration-300 ease focus:outline-none focus:border-red-400 hover:border-red-300 shadow-sm focus:shadow" : "peer w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pr-3 pl-10 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"}
                     />
                     <MailOutlined className='absolute top-1.5 left-1.5 bg-gray-700 text-white p-1.5 rounded text-sm' />
                 </div>
+                {adresseError && <div className="text-left text-red-500 text-xs">{adresseError}</div>}
             </div>
             <div className="flex justify-end gap-2">
                 <button onClick={handleSubmit} className='bg-green-500 hover:bg-green-600 text-white py-2 px-4 text-sm  rounded focus:outline-none focus:ring-2 focus:ring-green-500'>Suivant</button>
