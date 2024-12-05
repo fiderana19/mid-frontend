b66d13106245fdc4d858245c332789201b74dbdc
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-12-05 16:48:14 +0300

    feat: Fixed the bg in login and signup

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { EyeInvisibleOutlined, EyeOutlined, LockOutlined, MailOutlined, WarningFilled } from '@ant-design/icons';
 import MidLogo from '../assets/image/mid-logo.jpg';
+import Bg from '../assets/image/bg-login.jpeg'
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { LoginInterface } from '../interfaces/User';
@@ -56,8 +57,9 @@ function LoginPage() {
     return (
         <div className='min-h-screen flex flex-col justify-center'>
             <div className='sm:flex block justify-between'>
-                <div className="bg-[url('src/assets/image/bg-login.jpeg')] bg-cover lg:w-2/3 sm:w-1/2 w-full">
-                    <div className='flex flex-col justify-center h-screen bg-black bg-opacity-45'>
+                <div className="lg:w-2/3 sm:w-1/2 w-full relative">
+                    <img src={Bg} alt="" className='-z-50 absolute w-full h-full object-cover' />
+                    <div className='z-50 flex flex-col justify-center h-screen bg-black bg-opacity-45'>
                         <div className='text-white lg:px-20 sm:px-10 px-4'>
                             <img src={MidLogo} alt='Logo du ministere' className='w-28 h-28 object-cover' />
                             <div className='text-4xl font-latobold mt-5 mb-4'>MINISTERE DE L'INTERIEUR</div>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import MidLogo from '../assets/image/mid-logo.jpg'
+import MidLogo from '../assets/image/mid-logo.jpg';
+import Bg from '../assets/image/bg-login.jpeg';
 import SignupForm from './Signup/SignupForm';
 
 function Signup() {
@@ -18,8 +19,9 @@ function Signup() {
                         </Link>
                     </div>
                 </div>
-                <div className="lg:w-2/3 sm:w-1/2 w-full bg-[url('src/assets/image/bg-login.jpeg')] bg-cover">
-                    <div className='flex flex-col justify-center h-screen bg-black bg-opacity-45'>
+                <div className="lg:w-2/3 sm:w-1/2 w-full relative">
+                    <img src={Bg} alt="" className='-z-50 absolute w-full h-full object-cover' />
+                    <div className='z-50 flex flex-col justify-center h-screen bg-black bg-opacity-45'>
                         <div className='text-white lg:px-20 sm:px-10 px-4'>
                             <img src={MidLogo} alt='Logo du ministere' className='w-28 h-28 object-cover' />
                             <div className='text-4xl font-latobold mt-5 mb-4'>MINISTERE DE L'INTERIEUR</div>
