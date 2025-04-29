d361d72dd9fd3aa53fa1a72740ba36213fcccc5a
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-04-29 18:44:54 +0300

    feat: Upgraded the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
-import { AuthProvider } from './context/AuthContext.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AuthProvider } from './context/AuthContext.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AuthProvider>
+            <App />
+            <ReactQueryDevtools />
+        </AuthProvider>
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
