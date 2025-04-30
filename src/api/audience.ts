195aedb741715abeac86d545871ffb4a2d377b3b
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-04-30 14:29:43 +0300

    feat: Optimized the audience api call

--- src/api/audience.ts
+++ src/api/audience.ts
@@ -1,166 +1,43 @@
-import axios from "axios";
+import axiosAuthInstance from "./Config";
 
-const AudienceAPIUrl = "http://localhost:3002/audience";
+const AudienceAPIUrl = `${import.meta.env.VITE_BASE_URL}/audience`;
 
-export const getAllAudience = async (token: string | null) => {
-  try {
-    const response = await axios({
-      method: 'get',
-      url: `${AudienceAPIUrl}/all`,
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    return response;
-  } catch (error) {
-    console.error("Erreur lors de la récuperation des audiences :", error)
-  }
+export const getAllAudience = async () => {
+  return await axiosAuthInstance.get(`${AudienceAPIUrl}/all`);
 }
 
-export const getAudienceById = async (token: string | null, id:string) => {
-  try {
-    const response = await axios({
-      method: 'get',
-      url: `${AudienceAPIUrl}/get/${id}`,
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    return response;
-  } catch (error) {
-    console.error("Erreur lors de la recuperation de l'audience :", error)
-  }
+export const getAudienceById = async (id:string) => {
+  return await axiosAuthInstance.get(`${AudienceAPIUrl}/get/${id}`);
 }
 
-export const getAudienceByRef = async (token: string | null, id:string) => {
-  try {
-    const response = await axios({
-      method: 'get',
-      url: `${AudienceAPIUrl}/ref/${id}`,
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    return response;
-  } catch (error) {
-    console.error("Erreur lors de la recuperation de l'audience :", error)
-  }
+export const getAudienceByRef = async (id:string) => {
+  return await axiosAuthInstance.get(`${AudienceAPIUrl}/ref/${id}`);
 }
 
-export const getAudienceByUser = async (token: string | null, id:string) => {
-  try {
-    const response = await axios({
-      method: 'get',
-      url: `${AudienceAPIUrl}/user/${id}`,
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    return response;
-  } catch (error) {
-    console.error("Erreur lors de la recuperation de l'audience de l'utilisateur :", error)
-  }
+export const getAudienceByUser = async (id:string) => {
+  return await axiosAuthInstance.get(`${AudienceAPIUrl}/user/${id}`);
 }
 
-export const audienceCreate = async (token: string | null, audienceData: any) => {
-    try {
-      const response = await axios({
-        method: 'post',
-        url: `${AudienceAPIUrl}/create`,
-        data: audienceData,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error) {
-      console.error("Erreur lors de la creation del'audience :", error)
-    }
+export const audienceCreate = async (audienceData: any) => {
+  return await axiosAuthInstance.post(`${AudienceAPIUrl}/create`, audienceData);
   }
 
-  export const audienceReport = async (token: string | null, id: string, audienceData: any) => {
-    try {
-      const response = await axios({
-        method: 'patch',
-        url: `${AudienceAPIUrl}/report/${id}`,
-        data: audienceData,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error) {
-      console.error("Erreur lors du report de l'audience :", error)
-    }
-  }
+export const audienceReport = async (id: string, audienceData: any) => {
+  return await axiosAuthInstance.patch(`${AudienceAPIUrl}/report/${id}`, audienceData);
+}
 
-  export const audienceCancel = async (token: string | null, id: string) => {
-    try {
-      const response = await axios({
-        method: 'patch',
-        url: `${AudienceAPIUrl}/cancel/${id}`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error) {
-      console.error("Erreur lors de l'annulation de l'audience :", error)
-    }
-  }
+export const audienceCancel = async (id: string) => {
+  return await axiosAuthInstance.patch(`${AudienceAPIUrl}/cancel/${id}`);
+}
 
-  export const audienceSearch = async (token: string | null, searchData: any) => {
-    try {
-      const response = await axios({
-        method: 'post',
-        url: `${AudienceAPIUrl}/search`,
-        data: searchData,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error) {
-      console.error("Erreur lors de l'annulation de l'audience :", error)
-    }
-  }
+export const audienceSearch = async (searchData: any) => {
+  return await axiosAuthInstance.post(`${AudienceAPIUrl}/search`, searchData);
+}
 
-  export const audienceClose = async (token: string | null, id: string) => {
-    try {
-      const response = await axios({
-        method: 'patch',
-        url: `${AudienceAPIUrl}/close/${id}`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error) {
-      console.error("Erreur lors de la classification de l'audience :", error)
-    }
-  }
+export const audienceClose = async (id: string) => {
+  return await axiosAuthInstance.patch(`${AudienceAPIUrl}/close/${id}`);
+}
 
-  export const audienceMissed = async (token: string | null, id: string) => {
-    try {
-      const response = await axios({
-        method: 'patch',
-        url: `${AudienceAPIUrl}/missed/${id}`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error) {
-      console.error("Erreur lors de la absentation de l'audience :", error)
-    }
-  }
+export const audienceMissed = async (id: string) => {
+  return await axiosAuthInstance.get(`${AudienceAPIUrl}/missed/${id}`);
+}
