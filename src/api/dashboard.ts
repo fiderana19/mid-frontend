b48c6895456c06fe3cf74f57d93bf41d201b5f08
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-04-30 14:40:14 +0300

    feat: Optimized the dashboard api call

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -1,91 +1,25 @@
-import axios from "axios";
+import axiosAuthInstance from "./Config";
 
-const AudienceAPIUrl = "http://localhost:3002/audience";
-const RequestAPUrl = "http://localhost:3002/request"
-const UserAPIUrl = "http://localhost:3002/auth";
+const AudienceAPIUrl = `${import.meta.env.VITE_BASE_URL}/audience`;
+const RequestAPUrl = `${import.meta.env.VITE_BASE_URL}/request`;
+const UserAPIUrl = `${import.meta.env.VITE_BASE_URL}/auth`;
 
-export const getUserStat = async (token: string |null) => {
-    try {
-        const response = await axios({
-          method: 'get',
-          url: `${UserAPIUrl}/chart`,
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        })
-    
-        return response;
-      } catch (error: any) {
-        console.error("Erreur lors de la recuperation des stat user :", error)
-        return error;
-      }
+export const getUserStat = async () => {
+  return await axiosAuthInstance.get(`${UserAPIUrl}/chart`);
 }
 
-export const getLatestUser = async (token: string |null) => {
-  try {
-      const response = await axios({
-        method: 'get',
-        url: `${UserAPIUrl}/latest`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error: any) {
-      console.error("Erreur lors de la recuperation des stat user :", error)
-      return error;
-    }
+export const getLatestUser = async () => {
+  return await axiosAuthInstance.get(`${UserAPIUrl}/latest`);
 }
 
-export const getRequestChart = async (token: string |null) => {
-  try {
-      const response = await axios({
-        method: 'get',
-        url: `${RequestAPUrl}/chart`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error: any) {
-      console.error("Erreur lors de la recuperation des stat request :", error)
-      return error;
-    }
+export const getRequestChart = async () => {
+  return await axiosAuthInstance.get(`${RequestAPUrl}/chart`);
 }
 
-export const getAudienceChart = async (token: string |null) => {
-  try {
-      const response = await axios({
-        method: 'get',
-        url: `${AudienceAPIUrl}/chart`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error: any) {
-      console.error("Erreur lors de la recuperation des stat audience :", error)
-      return error;
-    }
+export const getAudienceChart = async () => {
+  return await axiosAuthInstance.get(`${AudienceAPIUrl}/chart`);
 }
 
-export const getAudienceLast = async (token: string |null) => {
-  try {
-      const response = await axios({
-        method: 'get',
-        url: `${AudienceAPIUrl}/last`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-  
-      return response;
-    } catch (error: any) {
-      console.error("Erreur lors de la recuperation de la derniere audience :", error)
-      return error;
-    }
+export const getAudienceLast = async () => {
+  return await axiosAuthInstance.get(`${AudienceAPIUrl}/last`);
 }
-
